6022136b9a69c86409f67b5a3ad722500efef554
jasperbarcelona <barcelona_jasper@yahoo.com>
2014-07-14 17:49:55 +0800

    awesome

--- jaspe.py
+++ jaspe.py
@@ -18,6 +18,29 @@ db = SQLAlchemy(app)
 app.secret_key = '234234rfascasascqweqscasefqwefe2323234dvsv'
 ingredients=[]
 
+FACEBOOK_APP_ID = '539244542865228'
+FACEBOOK_APP_SECRET = 'efaee0037f9320831895a5e9aa4d1bc6'
+
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('email, ')}
+)
+
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_token')
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('facebook_token', None)
+
 
 class recipe3(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -371,6 +394,24 @@ def delete_from_list():
     del session['iddel'][deletfromList]
 
 
+@app.route("/facebook_login")
+def facebook_login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next'), _external=True))
+
+@app.route("/facebook_authorized")
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None or 'access_token' not in resp:
+        return redirect(next_url)
+
+    session['logged_in'] = True
+    session['facebook_token'] = (resp['access_token'], '')
+
+    return redirect(next_url)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index_route():
     if not session:
@@ -403,7 +444,8 @@ def add():
 
 @app.route('/logout')
 def logout():
-    empty_sessions()     
+    empty_sessions() 
+    pop_login_session()    
     return redirect("/")
 
 
